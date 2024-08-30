687f440436a59716661960a89085b92ad3e78830
Mark Faust <markfaust123@gmail.com>
2024-08-29 20:16:33 -0400

    created database and now connecting to it upon server intialization

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const port = process.env.port || 5001;
 
+connectDB(); // Connect to MongoDB
 const app = express();
 
 app.get("/", (req, res) => {
